bfaa327ad146772e9022be78d1146c3ecc40d7c7
Diwas Lamsal <diwaslamsal123@hotmail.com>
2019-04-02 10:15:35 +0545

    new

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 // When window loads
 $( window ).on("load", function() {
 	loaded();
-	cursorEffect();
 	removeLoader();
-	$('li').on('hover', function(){
-		console.log("asd");
-	});
-
 });
 
 // Function to remove Loader
@@ -17,7 +12,7 @@ function removeLoader(){
 
 	}, 1000);
 
- setTimeout($('.loader').css("display", "none"), 3000);
+ $('.loader').css("display", "none");
 
 }
 
@@ -30,42 +25,6 @@ function loaded(){
 }
 
 
-// Cursor effect
-function cursorEffect(){
-
-
-
-if(window.innerWidth>700){
-
-	var timer;
-   $(document).on('mousemove', function(e){
-		 if(window.scrollY == 0){
-			 clearInterval(timer);
-       timer = setTimeout(function() {
-           $('.bubble').animate({opacity:0}, 400, function() {
-               $(this).remove();
-           });
-       }, 10);
-
-
-			 var randomColor = "rgb("+getRandomNumber(0, 255)+", "+getRandomNumber(0, 255)+", "+getRandomNumber(0, 255)+")";
-
-       var t  = (e.pageY - 10).toString() + 'px',
-           l  = (e.pageX - 10).toString() + 'px',
-           el = $('<div />', {'class':'bubble',
-							 style: 'margin-left:' + l + ';margin-top:' + t + '; background: '+randomColor
-							 , text:''});
-
-       $('#intro').prepend(el);
-
-		 }
-   });
-
-	}//End of if(window.innerWidth>700)
-}//End of function cursorEffect
-
-
-
 // Function to scroll to a container
 // Works only with Desktop
 function scrollFunction(idVar){
@@ -73,29 +32,21 @@ function scrollFunction(idVar){
 	$('html, body').animate({
     scrollTop: idVar == 'intro' ? 0 : $('#'+idVar).offset().top
 	}, 500);
-
 	// Set all bullets to blank
 	var nav = document.getElementsByClassName('nav-pc')[0];
 	var bullets = nav.getElementsByTagName('li');
 	for (var i = 0; i < bullets.length; i++) {
     bullets[i].className = "";
 	}
-
 	// add the li for bullet's id
 	var bullet = idVar+'li';
 	var bulletLi = document.getElementById(bullet);
 	//Set clicked bullet to selected
 	bulletLi.className = "selected";
-
 }
+
 
 // Get random number between min and max
 function getRandomNumber(min, max) {
  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-
-//Change image
-function changeImage(imageName, imageTag){
-	setTimeout(function(){imageTag.src = imageName;}, 100);
 }
